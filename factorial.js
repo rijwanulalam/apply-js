c1c9a534548a0da33644c299a22a6e9a65084ba6
rijwanulalam <rijwanul007@gmail.com>
2023-11-07 16:22:42 +0600

    factorial with while loop

--- factorial.js
+++ factorial.js
@@ -1,9 +1,21 @@
+// function factorial(number){
+//     let result = 1;
+//     for(let i = number; i >= 1; i--){
+//         result = result * i;
+//     }
+//     return result;
+// }
+
+// console.log(factorial(4));
+
 function factorial(number){
     let result = 1;
-    for(let i = number; i >= 1; i--){
+    let i = 1;
+    while( i <= number){
         result = result * i;
+        i++;
     }
     return result;
 }
 
-console.log(factorial(4));
+console.log(factorial(3));
